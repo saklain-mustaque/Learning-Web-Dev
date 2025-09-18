92415161eccf86a581d34d3d9175153cbd3f1148
saklain-mustaque <smustaque89@gmal.com>
2025-09-18 19:21:44 +0530

    update package.json file for deployment

--- React/Profile Card Component/vite.config.js
+++ React/Profile Card Component/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Learning-Web-Dev/React/Profile-Card-Component/",
   plugins: [react(),
     tailwindcss(),
   ],
